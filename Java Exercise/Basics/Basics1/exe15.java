d36e9f8ae19a69fb22a3fbfee72853daf265b2d3
2000pawan <yaduvanshi2000pawan@gmail.com>
2024-09-15 23:27:20 +0530

    Commit

---
+++ Java Exercise/Basics/Basics1/exe15.java
@@ -0,0 +1,12 @@
+public class exe15 {
+    public static void main(String [] arg){
+        int a,b,temp;
+        a=10;
+        b=24;
+        System.out.println("Before swapping : a, b = " + a + ", " + b);
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After swapping : a, b = " + a + ", " + b);
+    }
+}
