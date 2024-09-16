08e061520a759f4fab7ea0e4849edc1d4c8437b9
2000pawan <yaduvanshi2000pawan@gmail.com>
2024-09-16 15:29:24 +0530

    Commit

---
+++ Java Exercise/Basics/Basics1/exe16.java
@@ -0,0 +1,9 @@
+public class exe16{
+    public static void main(String [] arg){
+        System.out.println(" +\"\"\"\"\"+ ");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  | ");
+        System.out.println(" | '-' | ");
+        System.out.println(" +-----+ ");
+    }
+}
